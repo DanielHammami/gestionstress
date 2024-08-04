38b94810f940ef6f516401ae8c93110f1fbec6db
Daniel Hammami <daniel.hammami@yahoo.com>
2024-08-04 11:55:28 +0200

    Création de la route userRoutes

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+const router = Router();
+
+// Inscription
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    const errorMessage =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    res.status(500).json({ message: errorMessage });
+  }
+});
+
+// Connexion
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+    res.json({ token, user });
+  } catch (err) {
+    const errorMessage =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    res.status(500).json({ message: errorMessage });
+  }
+});
+
+export default router;
